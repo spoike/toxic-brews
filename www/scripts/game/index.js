8b236deff6972d5c598a76d7706eb659946d2112
Mikael Brassman <mishakun@gmail.com>
2014-03-16 15:34:30 +0100

    Can now select items to combine

--- www/scripts/game/index.js
+++ www/scripts/game/index.js
@@ -2,7 +2,7 @@ define(['knockout', 'lodash'], function(ko, _) {
 
     function CombineItem(parent) {
         this.isSelected = ko.observable(false);
-        this.item = null;
+        this.item = ko.observable();
         this.parent = parent;
     }
 
@@ -23,6 +23,16 @@ define(['knockout', 'lodash'], function(ko, _) {
         this.isInventoryOpen = ko.observable(false);
 
         document.addEventListener('closeInventory', _.bind(function(e) {
+            this.isInventoryOpen(false);
+            this.resetSelect();
+        }, this), false);
+
+        document.addEventListener('selectInventoryItem', _.bind(function(e) {
+            var slot = _.find(this.itemsToCombine, function(combineItem) {
+                return combineItem.isSelected();
+            });
+            slot.item(e.detail);
+            console.log(e);
             this.isInventoryOpen(false);
             this.resetSelect();
         }, this), false);
--- www/scripts/game/inventory.js
+++ www/scripts/game/inventory.js
@@ -1,37 +1,41 @@
 define(['knockout', 'lodash'], function(ko, _) {
 
+    function InventoryItem(item, inventory) {
+        this.id = item.id;
+        this.count = 1;
+        this.item = item;
+        this.inventory = inventory;
+    }
+
+    InventoryItem.prototype.select = function() {
+        var evt = new CustomEvent('selectInventoryItem', {
+            'detail': this
+        });
+        document.dispatchEvent(evt);
+        this.inventory.unselect();
+    };
+
     function Inventory() {
         this.items = ko.observableArray();
-        this.items.push({
+        this.items.push(new InventoryItem({
             id: 'nirnroot',
-            count: 1,
-            item: {
-                id: 'nirnroot',
-                title: 'Nirnroot',
-                description: 'A common yet ugly looking root with many uses'
-            },
-            inventory: this
-        });
-        this.items.push({
+            title: 'Nirnroot',
+            description: 'A common yet ugly looking root with many uses'
+        }, this));
+        this.items.push(new InventoryItem({
             id: 'deathcap',
-            count: 2,
-            item: {
-                id: 'deathcap',
-                title: 'Death Cap',
-                description: 'Looks like a colorful mushroom, but deadly'
-            },
-            inventory: this
-        });
-        this.items.push({
+            title: 'Death Cap',
+            description: 'Looks like a colorful mushroom, but deadly'
+        }, this));
+        this.items.push(new InventoryItem({
             id: 'vampbite',
-            count: 3,
-            item: {
-                id: 'vampbite',
-                title: 'Vampire\'s Bite',
-                description: 'A harrowing flower, you can feel its blood lust coming from the petals'
-            },
-            inventory: this
-        });
+            title: 'Vampire\'s Bite',
+            description: 'A harrowing flower, you can feel its blood lust coming from the petals'
+        }, this));
+
+        this.items()[0].count = 5;
+        this.items()[1].count = 3;
+        this.items()[2].count = 2;
 
         this.selectedItem = ko.observable();
 
@@ -44,9 +48,13 @@ define(['knockout', 'lodash'], function(ko, _) {
         this.inventory.selectedItem(this);
     };
 
+    Inventory.prototype.unselect = function() {
+        this.selectedItem(null);
+    };
+
     Inventory.prototype.closeInventory = function() {
         var evt = new Event('closeInventory');
-        this.selectedItem(null);
+        this.unselect();
         document.dispatchEvent(evt); 
     };
 
